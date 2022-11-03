0ad5910f91f4365cdba262d5df63c88237032338
euni-yi <ejky53@gmail.com>
2022-11-03 11:21:08 -0600

    last two min

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Script.js
+
+// html elements
+let calcBtn = document.getElementById("calc-btn")
+let sumOut = document.getElementById("sum-out")
+
+// add event listner
+calcBtn.addEventListener("click", calcSum);
+
+// Event function
+function calcSum(){
+    // sum integers 1 to 10-0
+    let total = 0;
+   for(let n = 1;n<=100; n++){
+    total = total+n
+   }
+//    Ouutput the total
+   sumOut.innerHTML =total;
+}
