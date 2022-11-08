84a1f57f9ba34d4d08e2ec18a0e40635c7e011ad
euni-yi <113129884+euni-yi@users.noreply.github.com>
2022-11-08 10:09:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // html elements
 let calcBtn = document.getElementById("calc-btn")
 let sumOut = document.getElementById("sum-out")
+let input= document.getElementById("n-in");
+let output = document.getElementById("n-out")
+
 
 // add event listner
 calcBtn.addEventListener("click", calcSum);
@@ -11,9 +14,10 @@ calcBtn.addEventListener("click", calcSum);
 function calcSum(){
     // sum integers 1 to 10-0
     let total = 0;
-   for(let n = 1;n<=100; n++){
+   for(let n = 1;n<=output; n++){
     total = total+n
    }
+
 //    Ouutput the total
    sumOut.innerHTML =total;
 }
